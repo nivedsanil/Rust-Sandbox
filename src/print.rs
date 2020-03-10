132c49e71383a5f0413a1fd5ac64c3382c8afd07
nivedsanil <nivedsanil@hotmail.com>
2020-03-10 11:00:34 +0530

    Initial Commit

---
+++ src/print.rs
@@ -0,0 +1,9 @@
+pub fn run() {
+
+    println!("Hello From print.rs file");
+    println!("{} is from {}", "Nived", "Kerala");
+
+    // Positional Arguments
+
+    println!("{0} is from {1} and {0} likes to {2}", "Nived", "Kerala", "code")
+}
